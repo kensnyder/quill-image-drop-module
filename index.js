e8336045074845f86842e77746ae3d0be6350f53
Honesty1997 <honesty1997>
2018-11-13 00:17:43 +0800

    Add support for IE.

--- index.js
+++ index.js
@@ -19,6 +19,15 @@ export class ImageDrop {
 		// listen for drop and paste events
 		this.quill.root.addEventListener('drop', this.handleDrop, false);
 		this.quill.root.addEventListener('paste', this.handlePaste, false);
+
+		// when drag image over the drag zone, ie will open the file as default behavior.
+		// need to prevent the behavior so that the drop event will triggered correctly.
+		if (ImageDrop.isIE()) {
+			this.quill.root.addEventListener('dragover', evt => {
+				evt.stopPropagation();
+				evt.preventDefault();
+			});
+		}
 	}
 
 	/**
@@ -44,8 +53,10 @@ export class ImageDrop {
 	 * @param {Event} evt
 	 */
 	handlePaste(evt) {
-		if (evt.clipboardData && evt.clipboardData.items && evt.clipboardData.items.length) {
-			this.readFiles(evt.clipboardData.items, dataUrl => {
+		// Internet explorer's clipbaordData is under window object.
+		if (evt.clipboardData && evt.clipboardData.items && evt.clipboardData.items.length
+			 || window.clipboardData && window.clipboardData.files && window.clipbaordData.files.length) {
+			this.readFiles(evt.clipboardData.items || window.clipboardData.files, dataUrl => {
 				const selection = this.quill.getSelection();
 				if (selection) {
 					// we must be in a browser that supports pasting (like Firefox)
@@ -95,4 +106,33 @@ export class ImageDrop {
 		});
 	}
 
+	/**
+	 * detect IE
+	 * returns version of IE or false, if browser is not Internet Explorer
+	 */
+	static ieIE() {
+		const ua = window.navigator.userAgent;
+
+		const msie = ua.indexOf('MSIE ');
+		if (msie > 0) {
+			// IE 10 or older => return version number
+			return parseInt(ua.substring(msie + 5, ua.indexOf('.', msie)), 10);
+		}
+
+		const trident = ua.indexOf('Trident/');
+		if (trident > 0) {
+			// IE 11 => return version number
+			var rv = ua.indexOf('rv:');
+			return parseInt(ua.substring(rv + 3, ua.indexOf('.', rv)), 10);
+		}
+
+		const edge = ua.indexOf('Edge/');
+		if (edge > 0) {
+			// Edge (IE 12+) => return version number
+			return parseInt(ua.substring(edge + 5, ua.indexOf('.', edge)), 10);
+		}
+
+			// other browser
+			return false;
+	}
 }
