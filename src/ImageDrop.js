98c3b7928b7cb1858f86f32edd9834786239fef1
Manan Jadhav <curosmj@gmail.com>
2019-10-19 12:40:09 -0400

    Fix image paste to work with system screenshot tools
    The image paste function was not working when pasting through the system-installed screenshot tools such as Lightshot, Grab (Mac), Gyazo.

--- src/ImageDrop.js
+++ src/ImageDrop.js
@@ -44,18 +44,10 @@ export class ImageDrop {
 	 * @param {Event} evt
 	 */
 	handlePaste(evt) {
+		evt.preventDefault();
 		if (evt.clipboardData && evt.clipboardData.items && evt.clipboardData.items.length) {
 			this.readFiles(evt.clipboardData.items, dataUrl => {
-				const selection = this.quill.getSelection();
-				if (selection) {
-					// we must be in a browser that supports pasting (like Firefox)
-					// so it has already been placed into the editor
-				}
-				else {
-					// otherwise we wait until after the paste when this.quill.getSelection()
-					// will return a valid index
-					setTimeout(() => this.insert(dataUrl), 0);
-				}
+				setTimeout(() => this.insert(dataUrl), 0);
 			});
 		}
 	}
